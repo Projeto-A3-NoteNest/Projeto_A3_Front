ebdf3c1b9737617b0c933d32e0e90c24c301daf0
Z0catelli <gabrielazge@gmail.com>
2023-11-07 23:39:22 -0300

    add js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnSegunda = document.querySelector('#btn-segunda');
+
+btnSegunda.addEventListener('click', () => {
+  console.log('funcionou')
+})
+
+
+//pesquisar como atrelar o div em elementos DOM
